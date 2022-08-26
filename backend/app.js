a1a8591c6e32f7c4daf517bee236c15895097d63
shreyguptaiitg <shreygupta@iitg.ac.in>
2022-08-26 23:10:15 +0530

    deleted key

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 mongoose
   .connect(
-    "mongodb+srv://shreyguptaiitg:Ramsagar%40123@cluster0.4j1trac.mongodb.net/test"
+    ""
   )
   .then(() => app.listen(5000))
   .then(() =>
